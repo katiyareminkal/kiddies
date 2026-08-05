19f2c358e1e495b96a923caf84ba8cf2e332aa66
katiyareminkal <katiyareminkal@gmail.com>
2026-08-06 03:09:40 +0530

    fix: add GlobalErrorBoundary, chunk error recovery, and no-cache headers to eliminate white screen issues

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { GlobalErrorBoundary } from './components/ErrorBoundary';
 import { registerSW } from 'virtual:pwa-register';
 
 // Register standard PWA service worker after page load
@@ -19,6 +20,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalErrorBoundary>
+      <App />
+    </GlobalErrorBoundary>
   </React.StrictMode>
 );
